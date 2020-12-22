fcddcc6d894b5c931c22a7caf4a7aef21c3fd279
nguyenthitram2810 <nguyenthitram2010a@gmail.com>
2020-12-22 13:16:04 +0700

    list job of company

--- pages/home/job/index.vue
+++ pages/home/job/index.vue
@@ -15,38 +15,42 @@
       <div class="tab-pane fade show active" id="contact" role="tabpanel" aria-labelledby="contact-tab">
   	    <div class="box shadow-sm border rounded bg-white p-3">
   		    <div class="row">
-  			    <div class="col-md-6">
-  				    <a href="job-profile.html">
+  			    <div v-for="(item, index) in data" :key="index" class="col-12">
+  				    <div>
   					    <div class="border job-item mb-3">
-  						    <div class="d-flex align-items-center p-3 job-item-header">
+  						    <div class="d-flex align-items-center justify-content-between p-3 job-item-header">
   							    <div class="overflow-hidden mr-2">
-  							  	  <h6 class="font-weight-bold text-dark mb-0 text-truncate">UI/UX designer</h6>
-  							  	  <div class="text-truncate text-primary">Envato</div>
+  							  	  <nuxt-link :to="`/job/${item.id}`"><h6 class="font-weight-bold text-dark mb-0 text-truncate">{{ item.name }}</h6></nuxt-link>
   							  	  <div class="small text-gray-500">
-  							  		  <i class="feather-map-pin"></i> India, Punjab
+  							  		  <i class="feather-map-pin"></i> {{ item.address.description }}
   							  	  </div>
   							    </div>
-  							    <img class="img-fluid ml-auto" src="img/l1.png" alt="">
+  							    <a-button shape="round" type="danger" size="small" class="font--13">
+                      {{ item.type }}
+                    </a-button>
   					  	  </div>
 
+                  <div class="w-100 px-3 pb-2 ml-1 row">
+                    <button v-for="(e, index) in item.categories" :key="index" type="button" class="mr-2 mb-2 btn btn-light d-flex align-items-center">
+                      {{ e.name }}
+                    </button>
+                  </div>
+
   							  <div class="d-flex align-items-center p-3 border-top border-bottom job-item-body">
-  							  	<div class="overlap-rounded-circle">
-  								  	<img class="rounded-circle shadow-sm" src="/images/p1.png" alt="" data-original-title="Sophia Lee">
-  										<img class="rounded-circle shadow-sm" src="/images/p2.png" alt="" data-original-title="John Doe">
-  										<img class="rounded-circle shadow-sm" src="/images/p3.png" alt="" data-original-title="Julia Cox">
-  										<img class="rounded-circle shadow-sm" src="/images/p4.png" alt="" data-original-title="Robert Cook">
-  										<img class="rounded-circle shadow-sm" src="/images/p5.png" alt="" data-original-title="Sophia Lee">
-  									</div>
-  									<span class="font-weight-bold text-primary">18 connections</span>
+  								  <img width="100%" height="100%" :src="item.introImg" alt="" data-original-title="Sophia Lee">
   							  </div>
 
-  								<div class="p-3 job-item-footer">
-  									<small class="text-gray-500">
-  										<i class="feather-clock"></i> Posted 3 Days ago
+  								<div class="p-3 job-item-footer d-flex align-items-center justify-content-between">
+  									<small class="">
+  										<i class="feather-clock"></i> Posted: {{changeStringToTime(item.createdat)}}
+  									</small>
+
+                    <small class="">
+  										<i class="feather-clock"></i> Deadline: {{changeStringToTime(item.deadline)}}
   									</small>
   								</div>
   							</div>
-  						</a>
+              </div>
   					</div>
           </div>
         </div>
